92071f58642efc73317d27ee3b8505a3fa25f16e
Misa <arensiatik@hotmail.com>
2014-02-19 00:18:26 -0600

    Add template por menu

---
+++ incubu/static/js/menu.js
@@ -0,0 +1,12 @@
+$(document).ready(
+  function () {
+    $('.nav li').hover(
+      function () { //appearing on hover
+        $('ul', this).fadeIn();
+      },
+      function () { //disappearing on hover
+        $('ul', this).fadeOut();
+      }
+    );
+  }
+);
